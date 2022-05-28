c43a4e3ed7e09c1c8f0c06b106a5d63595f8578b
Flu3nt <gnedyalkov94@gmail.com>
2022-05-28 18:03:27 +0300

    Added a player movement

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const c = canvas.getContext('2d');
 canvas.width = innerWidth; // innerWidth comes from the window object from the browser
 canvas.height = innerHeight;
 
-class Boudary {
+class Boundary {
     static width = 40;
     static height = 40;
     constructor({ position }) {
@@ -25,6 +25,56 @@ class Boudary {
     }
 }
 
+class Player {
+    constructor({ position, velocity }) {
+        this.position = position;
+        this.velocity = velocity;
+        this.radius = 15;
+    }
+
+    draw() {
+        c.beginPath();
+        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+        c.fillStyle = 'yellow';
+        c.fill();
+        c.closePath();
+    }
+
+    update() {
+        this.draw();
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+    }
+}
+
+const boundaries = [];
+const player = new Player({
+    position: {
+        x: Boundary.width + Boundary.width / 2,
+        y: Boundary.height + Boundary.height / 2,
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    }
+});
+
+const keys = {
+    w: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    },
+    s: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    }
+
+}
+
 const map = [
     ['-', '-', '-', '-', '-', '-'],
     ['-', ' ', ' ', ' ', ' ', '-'],
@@ -33,17 +83,15 @@ const map = [
     ['-', '-', '-', '-', '-', '-'],
 ];
 
-const boundaries = [];
-
 map.forEach((row, i) => {
     row.forEach((symbol, j) => {
         switch (symbol) {
             case '-':
                 boundaries.push(
-                    new Boudary({
+                    new Boundary({
                         position: {
-                            x: Boudary.width * j,
-                            y: Boudary.height * i
+                            x: Boundary.width * j,
+                            y: Boundary.height * i
                         }
                     })
                 )
@@ -52,6 +100,57 @@ map.forEach((row, i) => {
     });
 });
 
-boundaries.forEach(boudary => {
-    boudary.draw();
+function animate() {
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, canvas.width, canvas.height);
+
+    boundaries.forEach(boudary => {
+        boudary.draw();
+    });
+
+    player.update();
+
+}
+
+animate();
+
+// player movement
+addEventListener('keydown', ({ key }) => {
+    switch (key) {
+        case 'w':
+            keys.w.pressed = true;
+            break;
+        case 'a':
+            keys.a.pressed = true;
+            break;
+        case 's':
+            keys.s.pressed = true;
+            break;
+        case 'd':
+            keys.d.pressed = true;
+            break;
+    }
+
+    console.log(player.d.pressed);
+    console.log(player.s.pressed);
+});
+
+addEventListener('keyup', ({ key }) => {
+    switch (key) {
+        case 'w':
+            keys.w.pressed = false;
+            break;
+        case 'a':
+            keys.a.pressed = false;
+            break;
+        case 's':
+            keys.s.pressed = false;
+            break;
+        case 'd':
+            keys.d.pressed = false;
+            break;
+    }
+
+    console.log(player.d.pressed);
+    console.log(player.s.pressed);
 });
